df64520082d2cc050a54e06e855cd18a89b7c3ed
Nailuj <me@nailuj.dev>
2025-12-02 00:06:11 -0500

    Day 2 part 1

--- src/NewDay.kt
+++ src/NewDay.kt
@@ -2,7 +2,7 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 
-const val DAY = 1
+const val DAY = 2
 
 fun main() {
     var template = File("src/template.kt").readText()
