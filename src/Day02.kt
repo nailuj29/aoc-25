78eccb40692752f12e62511e370ad4d4147c6d31
Nailuj <me@nailuj.dev>
2025-12-02 00:14:50 -0500

    Day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,18 +1,61 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return -1
+    fun checkRepeats(string: String, num: Int): Boolean {
+        val chunks = string.chunked(num)
+
+        return chunks.toSet().size == 1
     }
 
-//    fun part2(input: List<String>): Int {
-//
-//    }
+    fun part1(input: List<String>): Long {
+        val line = input[0]
+        val ids = line.split(",").flatMap {
+            val (fst, snd) = it.split("-")
+            fst.toLong()..snd.toLong()
+        }
+
+        var total = 0L
+        for (id in ids) {
+            val str = id.toString()
+            val firstHalf = str.take(str.length / 2)
+            val secondHalf = str.drop(str.length / 2)
+
+            if (firstHalf == secondHalf) {
+                total += id
+            }
+        }
+
+        return total
+    }
+
+    fun part2(input: List<String>): Long {
+        val line = input[0]
+        val ids = line.split(",").flatMap {
+            val (fst, snd) = it.split("-")
+            fst.toLong()..snd.toLong()
+        }
+
+        var total = 0L
+        for (id in ids) {
+            val str = id.toString()
+
+            for (i in 1..str.length/2) {
+                if (str.length % i == 0) {
+                    if (checkRepeats(str, i)) {
+                        total += id
+                        break
+                    }
+                }
+            }
+        }
+
+        return total
+    }
 
     val testInput = readInput("day2_test")
 
-    check(part1(testInput) == 0)
-//    check(part2(testInput) == 0)
+    check(part1(testInput) == 1227775554L)
+    check(part2(testInput) == 4174379265L)
 
     val input = readInput("day2")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
