6ce4cfd6176376a4578b4b318a377b244b28f47f
Nailuj <me@nailuj.dev>
2025-12-04 16:43:59 -0500

    Day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,13 +1,13 @@
 fun main() {
-    fun check(grid: List<String>, coords: Pair<Int, Int>): Boolean {
+    fun check(grid: List<List<Boolean>>, coords: Pair<Int, Int>): Boolean {
         try {
-            return grid[coords.first][coords.second] == '@'
+            return grid[coords.first][coords.second]
         } catch (e: Exception) {
             return false
         }
     }
 
-    fun countNeighbors(grid: List<String>, coords: Pair<Int, Int>): Int {
+    fun countNeighbors(grid: List<List<Boolean>>, coords: Pair<Int, Int>): Int {
         var neighbors = 0
 
         for (i in -1..1) {
@@ -21,27 +21,47 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
+        val grid = input.map { it.map { c -> c == '@' } }
+
         var count = 0
 
-        for (i in input.indices) {
-            for (j in input[i].indices) {
-                if (input[i][j] == '@' && countNeighbors(input, i to j) < 4) count++
+        for (i in grid.indices) {
+            for (j in grid[i].indices) {
+                if (grid[i][j] && countNeighbors(grid, i to j) < 4) count++
             }
         }
 
         return count
     }
 
-//    fun part2(input: List<String>): Int {
-//
-//    }
+    fun part2(input: List<String>): Int {
+        val grid = input.map { it.map { c -> c == '@'}.toMutableList() }.toMutableList()
+        var count = 0
+
+        var didRemove = false
+        do {
+            didRemove = false
+
+            for (i in grid.indices) {
+                for (j in grid[i].indices) {
+                    if (grid[i][j] && countNeighbors(grid, i to j) < 4) {
+                        count++
+                        didRemove = true
+                        grid[i][j] = false
+                    }
+                }
+            }
+        } while (didRemove)
+
+        return count
+    }
 
     val testInput = readInput("day4_test")
 
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 43)
 
     val input = readInput("day4")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
